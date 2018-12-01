0d7754aa1d694c134de09179841836adb265c714
takabatoshiki <takabatoshiki@gmail.com>
2018-12-01 20:49:59 +0900

    add html

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next();
-} );
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log('In another middleware!');
+  res.status(404).send('<h1>Page not found</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
